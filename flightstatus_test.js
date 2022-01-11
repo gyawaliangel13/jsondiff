763d71cc59b8bbe4786fb3173174fa19f795679f
Angel Gyawali <angelgyawali@aa.com>
2022-01-11 00:35:39 -0600

    added jq

--- flightstatus_test.js
+++ flightstatus_test.js
@@ -37,7 +37,7 @@ jQuery(document).ready(function () {
           $.ajax(settings).done(function (response) {
             console.log(response);
           });
-
+          /// this was added
 
     }
     
